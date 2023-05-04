f3241486c3d555768e9f42851c473727d5736fa2
BuildTools <unconfigured@null.spigotmc.org>
2023-05-04 20:07:06 +0800

    E

--- src/main/java/me/spongycat/autoreplantenchant/AutoReplantEnchant.java
+++ src/main/java/me/spongycat/autoreplantenchant/AutoReplantEnchant.java
@@ -1,17 +1,122 @@
 package me.spongycat.autoreplantenchant;
 
+import me.spongycat.autoreplantenchant.customenchant.AutoReplantEnchantment;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AutoReplantEnchant extends JavaPlugin {
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public final class AutoReplantEnchant extends JavaPlugin {
+    private static me.spongycat.autoreplantenchant.AutoReplantEnchant plugin;
+    public static AutoReplantEnchantment autoReplantEnchantment;
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
+        autoReplantEnchantment = new AutoReplantEnchantment("auto_replant");
 
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        registerEnchantment(autoReplantEnchantment);
+        this.getServer().getPluginManager().registerEvents(autoReplantEnchantment, this);
+
+        if (getConfig().getBoolean("Allow_Crafting")) {
+            ItemStack hoe = new ItemStack(Material.NETHERITE_HOE, 1);
+            hoe.addUnsafeEnchantment(AutoReplantEnchant.autoReplantEnchantment, 1);
+            hoe.addEnchantment(Enchantment.DIG_SPEED, 5);
+            hoe.addEnchantment(Enchantment.DURABILITY, 3);
+            hoe.addEnchantment(Enchantment.MENDING, 1);
+            hoe.addEnchantment(Enchantment.LOOT_BONUS_BLOCKS, 3);
+            ItemMeta meta = hoe.getItemMeta();
+            ArrayList<String> lore = new ArrayList<>();
+            lore.add(ChatColor.GOLD + "Auto Replant I");
+            meta.setLore(lore);
+            hoe.setItemMeta(meta);
+
+            ItemStack carrot = new ItemStack(Material.GOLDEN_CARROT, 1);
+            carrot.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 10);
+            ItemMeta meta2 = carrot.getItemMeta();
+            ArrayList<String> lore2 = new ArrayList<>();
+            lore2.add(ChatColor.LIGHT_PURPLE + "Special golden carrot with mysterious power...");
+            meta2.setLore(lore2);
+            meta2.setDisplayName(ChatColor.GOLD + "Super Golden Carrot");
+            carrot.setItemMeta(meta2);
+
+
+
+
+            ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "Replant_Hoe"), hoe);
+            recipe.shape("PXP", "XYX", "AAA");
+            recipe.setIngredient('X', carrot.getData());
+            recipe.setIngredient('Y', Material.NETHERITE_HOE);
+            recipe.setIngredient('A', Material.NETHERITE_INGOT);
+            recipe.setIngredient('P', Material.DISPENSER);
+
+            ShapedRecipe recipe2 = new ShapedRecipe(new NamespacedKey(this, "Super_Carrot"), carrot);
+            recipe2.shape("SSS", "SSS", "SSS");
+            recipe2.setIngredient('S', Material.GOLDEN_CARROT);
+
+            Bukkit.addRecipe(recipe);
+            Bukkit.addRecipe(recipe2);
+        }
     }
+
+    public static AutoReplantEnchant getPlugin() {
+        return plugin;
+    }
+
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        try {
+            Field keyField = Enchantment.class.getDeclaredField("byKey");
+
+            keyField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            HashMap<NamespacedKey, Enchantment> byKey = (HashMap<NamespacedKey, Enchantment>) keyField.get(null);
+
+            if(byKey.containsKey(autoReplantEnchantment.getKey())) {
+                byKey.remove(autoReplantEnchantment.getKey());
+            }
+            Field nameField = Enchantment.class.getDeclaredField("byName");
+
+            nameField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            HashMap<String, Enchantment> byName = (HashMap<String, Enchantment>) nameField.get(null);
+
+            if(byName.containsKey(autoReplantEnchantment.getName())) {
+                byName.remove(autoReplantEnchantment.getName());
+            }
+        } catch (Exception ignored) { }
     }
+
+    public static void registerEnchantment(Enchantment enchantment) {
+        boolean registered = true;
+        try {
+            Field f = Enchantment.class.getDeclaredField("acceptingNew");
+            f.setAccessible(true);
+            f.set(null, true);
+            Enchantment.registerEnchantment(enchantment);
+        } catch (Exception e) {
+            registered = false;
+            e.printStackTrace();
+        }
+        if(registered){
+            // It's been registered!
+        }
+    }
+
+
+
 }
--- src/main/java/me/spongycat/autoreplantenchant/customenchant/AutoReplantEnchantment.java
+++ src/main/java/me/spongycat/autoreplantenchant/customenchant/AutoReplantEnchantment.java
@@ -1,14 +1,75 @@
 package me.spongycat.autoreplantenchant.customenchant;
 
-import org.bukkit.NamespacedKey;
+import me.spongycat.autoreplantenchant.AutoReplantEnchant;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.material.Crops;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class AutoReplantEnchant extends Enchantment {
-    public AutoReplantEnchant() {
-        super(new NamespacedKey(me.spongycat.autoreplantenchant.AutoReplantEnchant.getPlugin(), "auto_replant"));
+import java.util.Arrays;
+import java.util.List;
+
+import static me.spongycat.autoreplantenchant.AutoReplantEnchant.autoReplantEnchantment;
+
+
+public class AutoReplantEnchantment extends Enchantment implements Listener {
+    public AutoReplantEnchantment(String namespace) {
+        super(new NamespacedKey(me.spongycat.autoreplantenchant.AutoReplantEnchant.getPlugin(), namespace));
     }
+
+    private final List<Material> cropList = Arrays.asList(Material.WHEAT, Material.POTATOES, Material.CARROTS, Material.BEETROOTS);
+    @EventHandler
+    public void breakEvent(BlockBreakEvent event) {
+
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        ItemStack tool = player.getInventory().getItemInMainHand();
+        Material cropBlockType;
+        if(tool.getEnchantments().containsKey(Enchantment.getByKey(autoReplantEnchantment.getKey()))) {
+
+            if (cropList.contains(block.getType())) {
+                cropBlockType = block.getType();
+
+            } else {
+                return;
+            }
+
+
+            // Main functionality of the plugin
+            if (isFullyGrown(block)) {
+                replantCrop(block.getLocation(), cropBlockType);
+                ItemStack itemInHand = player.getInventory().getItemInMainHand();
+                int oldDurability = itemInHand.getDurability();
+                itemInHand.setDurability((short) (oldDurability + 1));
+
+            }
+        }
+    }
+
+    public boolean isFullyGrown(Block block) {
+        // Check if it is fully grown
+        Ageable ageable = (Ageable) block.getBlockData();
+        int maximumAge = ageable.getMaximumAge();
+
+        return ageable.getAge() == maximumAge;
+    }
+    public void replantCrop(Location location, Material cropBlockType) {
+        Bukkit.getScheduler().runTaskLater(AutoReplantEnchant.getPlugin(), () -> {
+            location.getBlock().setType(cropBlockType);
+        },5L);
+    }
+
 
     @Override
     public String getName() {
